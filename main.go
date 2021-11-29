3a96793b60c407529e151014f0ca07b49020beb2
Student <fhgf9048@gmail.com>
2021-11-29 20:24:49 +0500

    Обработка URL - запросов в Golang

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
@@ -22,7 +24,21 @@ func home(w http.ResponseWriter, r *http.Request)  {
 
 // Обработчик для отображения сожержимого заметки.
 func showSnippet(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("Отображение заметки..."))
+	// Извлекаем значене параметра id из URL и попытаемся
+	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+	// кновертировать в integer, или значение меньше 1, возвращаем ответ
+	// 404 - страница не найдена!
+	id, err := strconv.Atoi(r.URL.Query().Get("id")) 
+	if err != nil || id < 1{
+		http.NotFound(w, r)
+		return
+	}
+
+	// Используем функцию fmt.Fprintf() для вставкизначения из id в строку ответа
+	// и записываем его в http.ResponseWriter
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d ...", id)
+
+	// w.Write([]byte("Отображение заметки..."))
 }
 
 // Обработчик для создания новой заметки.
