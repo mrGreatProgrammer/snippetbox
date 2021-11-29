40ba569181aabc4aa55f4bf98bb8c90982b989be
Student <fhgf9048@gmail.com>
2021-11-29 20:59:05 +0500

    Оргонизация веб-приложения на Go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -57,17 +56,4 @@ func createSnippet(w http.ResponseWriter, r *http.Request)  {
 		return
 	}
 	w.Write([]byte("Форма для создания новой заметки..."))
-}
-
-func main()  {
-	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
-	// маршрутизаторе servemux
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
---
+++ cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+
+func main()  {
+	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
+	// маршрутизаторе servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
