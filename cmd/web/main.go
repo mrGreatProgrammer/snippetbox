b4caa0e96f52807ea964d02251a41ae49c6a9c6e
Student <fhgf9048@gmail.com>
2021-12-02 15:41:56 +0500

    8 получаем доступ к статическим файлам - css и js

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	// "path/filepath"
+
 	"github.com/mrGreatProgrammer/snippetbox/cmd/web/handlers"
 )
 
+func main() {
+mux := http.NewServeMux()
+mux.HandleFunc("/", handlers.Home)
+mux.HandleFunc("/snippet", handlers.ShowSnippet)
+mux.HandleFunc("snippet/create", handlers.CreateSnippet)
 
+// Инициализируем FileServer, он будет обрабатывать
+// HTTP-запросы к статическим файлам из папки "./ui/static".
+// Обратите внимание, что переданный в финукцию http.Dir путь
+// является относительным корневой папке проекта
+fileServer := http.FileServer(http.Dir("ui/static/"))
 
-func main()  {
-	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
-	// маршрутизаторе servemux
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handlers.Home)
-	mux.HandleFunc("/snippet", handlers.ShowSnippet)
-	mux.HandleFunc("/snippet/create", handlers.CreateSnippet)
+// Используем функцию mux.Handle() для регистрации обработчика для
+// всех запросов, которые начинаются с "/static/". Мы Убираем
+// префикс "/static" перед тем как запрос достигнет http.FileServer
+mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+log.Println("Запуск сервера на http//loaclhost:4000")
+err := http.ListenAndServe(":4000", mux)
+log.Fatal(err)
 }
