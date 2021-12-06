0278c2e0c3399a85a7d8aac162057a520a1d34d0
Student <fhgf9048@gmail.com>
2021-12-06 10:19:31 +0500

    настройка веб-приложения из командной строки

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -9,22 +10,33 @@ import (
 )
 
 func main() {
+	// Создаем новый флаг командной строки, значение по умолчанию: ":4000".
+	// Добавляем небольшую справку, объясняющая, что содержит данный флаг.
+	// Значение флага будет сохранено в переменной addr.
+	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+
+	// МЫ вызываем функцию flag.Parse() для извлечение флага из командной строки.
+	// Она считывает значение флага из командной строки и присваивает его содержимое
+	// переменной. Вам нужно вызвать ее *до* использования переменной addr
+	// иначе она всегда будет содержать значение по умолчанию ":4000".
+	// Если есть ошибки во время извлечения данных - приложение будет остановлено.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.Home)
 	mux.HandleFunc("/snippet", handlers.ShowSnippet)
 	mux.HandleFunc("snippet/create", handlers.CreateSnippet)
 
-	// Инициализируем FileServer, он будет обрабатывать
-	// HTTP-запросы к статическим файлам из папки "./ui/static".
-	// Обратите внимание, что переданный в финукцию http.Dir путь
-	// является относительным корневой папке проекта
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("ui/static/")})
-
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Запуск сервера на http//loaclhost:4000")
-	err := http.ListenAndServe(":4000", mux)
+	// Значение, возвращает функцией flag.String(), является указателем на значение
+	// из флага, а не самим значением. Нам нужно убрать ссылку на указатель
+	// то есть перед использованием добавьте к нему префикс *. Обратите внимание, что мы используем
+	// функцию log.Printf() для записи логов в журнал работы нашего приложения.
+	log.Printf("Запуск сервера на %s\n", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
 
