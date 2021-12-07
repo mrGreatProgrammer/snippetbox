fe01c971973e65110d27b0ed10006049f6a028fd
Student <fhgf9048@gmail.com>
2021-12-07 16:28:20 +0500

    изоляция маршрутизации приложения в отдельный файл

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 
 	"github.com/mrGreatProgrammer/snippetbox/cmd/web/handlers"
 )
@@ -33,54 +32,14 @@ func main() {
 
 	flag.Parse()
 
-	// Используем методы из структуры в качестве обработчиков маршрутов
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.Home)
-	mux.HandleFunc("/snippet", app.ShowSnippet)
-	mux.HandleFunc("snippet/create", app.CreateSnippet)
-
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("ui/static/")})
-	mux.Handle("/static", http.NotFoundHandler())
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Инициализируем новую структуру http.Server. Мы устанавливаем поля Addr и Handler, так
-	// что сервер использует тот же сетевой адрес и маршуруты, что и раньше, и назначаем
-	// поле ErrorLog, чтобы сервер использовал наш логгер
-	// при возникновении проблем.
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.Routes(), // Вызов нового метода app.routes()
 	}
 
 	infolog.Printf("Запуск сервера на %s\n", *addr)
 	// Вызываем метод ListenAndServe() от нашей новой структуры http.Server
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
-}
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			cloeErr := f.Close()
-			if cloeErr != nil {
-				return nil, cloeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
 }
